eedf67ab85a657c1f5e7dd642d50853a72289535
Yul's Data <58765210+yulsdata@users.noreply.github.com>
2021-06-15 13:28:32 +1000

    Add files via upload

--- Data Modelling with Postgres/sql_queries.py
+++ Data Modelling with Postgres/sql_queries.py
@@ -8,58 +8,68 @@ time_table_drop = "DROP table if exists time"
 
 # CREATE TABLES
 
-songplay_table_create = (""" 
-CREATE TABLE IF NOT EXISTS songplays (songplay_id serial PRIMARY KEY, start_time time, user_id int, level text, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar)
-""")
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar)""")
 
-user_table_create = (""" 
-CREATE TABLE IF NOT EXISTS users (user_id int, first_name text, last_name text, gender text, level text)
-""")
-
-song_table_create = (""" 
+song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
-                                  song_id varchar PRIMARY KEY, 
+                                  song_id text PRIMARY KEY, 
                                   title varchar, 
-                                  artist_id varchar, 
+                                  artist_id text NOT NULL, 
                                   year int, 
-                                  duration FLOAT)
+                                  duration numeric)
 """)
 
-artist_table_create = (""" 
+artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
-                                    artist_id varchar, 
-                                    name text, 
+                                    artist_id text PRIMARY KEY, 
+                                    name varchar, 
                                     location varchar, 
-                                    latitude varchar, 
-                                    longitude varchar)
+                                    latitude float, 
+                                    longitude float)
 """)
 
 time_table_create = (""" 
-CREATE TABLE IF NOT EXISTS time (start_time time, hour int, day int, week int, month int, year int, weekday int)
+CREATE TABLE IF NOT EXISTS time (
+                                 start_time TIMESTAMP PRIMARY KEY, 
+                                 hour int, 
+                                 day int, 
+                                 week int, 
+                                 month int, 
+                                 year int, 
+                                 weekday int)
 """)
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+                                       songplay_id serial PRIMARY KEY, 
+                                        start_time time, 
+                                        user_id int NOT NULL, 
+                                        level varchar, 
+                                        song_id text, 
+                                        artist_id text, 
+                                        session_id int, 
+                                        location varchar, 
+                                        user_agent varchar)
+""")
+
 
 # INSERT RECORDS
 
 songplay_table_insert = (""" INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
-user_table_insert = (""" INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
+user_table_insert = (""" INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) on conflict (user_id) do nothing
 """)
 
-song_table_insert = (""" INSERT INTO songs (song_id, 
-                                            title, 
-                                            artist_id, 
-                                            year, 
-                                            duration) 
-                                            VALUES (%s, %s, %s, %s, %s)
-                                            on conflict (song_id) do nothing
+song_table_insert = (""" INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) on conflict (song_id) do nothing
 """)
 
-artist_table_insert = (""" INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
+artist_table_insert = (""" INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) on conflict (artist_id) do nothing
 """)
 
 
-time_table_insert = (""" INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)
+time_table_insert = (""" INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) on conflict (start_time) do nothing
 """)
 
 # FIND SONGS
